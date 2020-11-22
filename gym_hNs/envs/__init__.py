42b896003e5eb4f619a1a6f175d2083e890646c4
srvrc <siyar.r.vurucu@gmail.com>
2020-11-22 11:21:28 +0100

    renaming

--- gym_hNs/envs/__init__.py
+++
@@ -1,2 +0,0 @@
-from gym_hNs.envs.v1 import TDM
-from gym_hNs.envs.v1 import ControlledTDM
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='gym_hackNSlash',
+setup(name='gym_macm',
       version='0.0.1',
       install_requires=['gym']  # And any other dependencies foo needs
 )
